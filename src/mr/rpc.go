eae424aa403dbefde08f7bd6e6c6a9207b41c149
silencezlyq <975259383@qq.com>
2023-10-08 16:40:39 +0800

    test m

--- src/mr/rpc.go
+++ src/mr/rpc.go
@@ -6,8 +6,10 @@ package mr
 // remember to capitalize all names.
 //
 
-import "os"
-import "strconv"
+import (
+	"os"
+	"strconv"
+)
 
 //
 // example to show how to declare the arguments
@@ -23,7 +25,7 @@ type ExampleReply struct {
 }
 
 // Add your RPC definitions here.
-
+//just test network
 
 // Cook up a unique-ish UNIX-domain socket name
 // in /var/tmp, for the coordinator.
